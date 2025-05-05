21b765f4007e5c4c7654defbd49284872cd47dbf
Devashish <ishay.gaikwad@gmail.com>
2025-05-05 12:06:27 +0000

    chore: requirements for linux

---
+++ requirements.sh
@@ -0,0 +1,3 @@
+conda activate ltn;
+pip install h5py jupyter notebook flask joblib lxml matplotlib networkx pandas scikit-learn scikit-posthocs scipy seaborn xlrd arrow tqdm untangle;
+pip install openpyxl sqlalchemy==1.2.10 ;
